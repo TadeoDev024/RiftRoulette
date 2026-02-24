f33bcffe2679d4a220fa5739574a7a0c6b9aa31d
CeleMyBeloved1404 <Tadeo.12972@gmail.com>
2026-02-24 18:32:30 -0300

    222

--- dataimporter.cs
+++ dataimporter.cs
@@ -19,154 +19,112 @@ public class RiotDataService {
             using var conn = new MySqlConnection(_connectionString);
             await conn.OpenAsync();
 
-            // DICCIONARIO MASIVO: Cubre el 99% de las temáticas del juego
-            var tematicas = new List<(string key, string theme)> {
-                ("proyecto", "PROYECTO"), ("project", "PROYECTO"),
-                ("luna sangrienta", "Luna Sangrienta"), ("blood moon", "Luna Sangrienta"),
-                ("estrella oscura", "Estrella Oscura"), ("dark star", "Estrella Oscura"),
-                ("guardiana estelar", "Guardianas Estelares"), ("star guardian", "Guardianas Estelares"),
-                ("pijamada estelar", "Guardianas Estelares"), ("némesis estelar", "Guardianas Estelares"),
-                ("forajid", "Forajidos"), ("solo ante el peligro", "Forajidos"), ("high noon", "Forajidos"),
-                ("k/da", "K/DA"), ("kda", "K/DA"),
-                ("true damage", "True Damage"),
-                ("pentakill", "Pentakill"),
-                ("heartsteel", "HEARTSTEEL"),
-                ("arcade", "Arcade"), ("recreativa", "Arcade"), ("jefe de batalla", "Arcade"), ("battle boss", "Arcade"),
-                ("piscina", "Pool Party"), ("pool party", "Pool Party"), ("veranieg", "Pool Party"), ("canción del océano", "Pool Party"),
-                ("florecer espiritual", "Florecer Espiritual"), ("spirit blossom", "Florecer Espiritual"),
-                ("odisea", "Odisea"), ("odyssey", "Odisea"),
-                ("academia de combate", "Academia de Combate"),
-                ("onda espacial", "Onda Espacial"), ("space groove", "Onda Espacial"),
-                ("empíreo", "Empíreo"), ("empyrean", "Empíreo"),
-                ("peleador de almas", "Peleador de Almas"), ("soul fighter", "Peleador de Almas"),
-                ("tinta sombría", "Tinta Sombría"), ("inkshadow", "Tinta Sombría"),
-                ("luna nevada", "Luna Nevada"), ("snow moon", "Luna Nevada"),
-                ("caballero de la ceniza", "Caballero de la Ceniza"), ("ashen knight", "Caballero de la Ceniza"),
-                ("creador de mitos", "Creador de Mitos"), ("mythmaker", "Creador de Mitos"),
-                ("bestia lunar", "Bestia Lunar"),
-                ("deleite lunar", "Deleite Lunar"), ("dinamita", "Deleite Lunar"), ("firecracker", "Deleite Lunar"),
-                ("reinos mecha", "Reinos Mecha"), ("mecha kingdoms", "Reinos Mecha"),
-                ("rosa de cristal", "Rosa de Cristal"), ("rosa marchita", "Rosa de Cristal"), ("crystal rose", "Rosa de Cristal"),
-                ("galante", "Galante"), ("debonair", "Galante"),
-                ("ciudad del crimen", "Ciudad del Crimen"), ("mafios", "Ciudad del Crimen"), ("crime city", "Ciudad del Crimen"),
-                ("domador de monstruos", "Domador de Monstruos"),
-                ("noche de miedo", "Noche de Miedo"), ("fright night", "Noche de Miedo"),
-                ("embrujada", "Halloween"), ("zombi", "Halloween"), ("conde", "Halloween"), ("nosferatu", "Halloween"), ("calabaza", "Halloween"),
-                ("bendición invernal", "Invierno"), ("maravilla invernal", "Invierno"), ("día nevado", "Invierno"), ("elfo", "Invierno"), ("santa", "Invierno"), ("nieve", "Invierno"),
-                ("buscacorazones", "Buscacorazones"), ("cariñosit", "Buscacorazones"), ("cupido", "Buscacorazones"), ("heartseeker", "Buscacorazones"),
-                ("victorios", "Victoriosa"), ("victorious", "Victoriosa"),
-                ("campeonato", "Campeonato"), ("championship", "Campeonato"),
-                ("conquistador", "Conquistador"), ("conqueror", "Conquistador"),
-                ("aspirante", "Aspirante"), ("challenger", "Aspirante"),
-                ("hextech", "Hextech"),
-                ("prestigios", "Edición Prestigiosa"), ("prestigio", "Edición Prestigiosa"),
-                ("arcane", "Arcane"),
-                ("astronauta", "Astronauta"), ("astronaut", "Astronauta"),
-                ("abeja", "Abejitas"), ("abejita", "Abejitas"), ("bee", "Abejitas"),
-                ("porcelana", "Porcelana"), ("porcelain", "Porcelana"),
-                ("cafetería", "Cafetería"), ("cafe cuties", "Cafetería"),
-                ("chef", "Culinarios"), ("panader", "Culinarios"), ("carnicer", "Culinarios"), ("sushiman", "Culinarios"), ("cociner", "Culinarios"),
-                ("comando", "Comando"), ("commando", "Comando"),
-                ("cyber pop", "Cyber Pop"),
-                ("definitivamente no", "Definitivamente No"),
-                ("dracomante", "Dracomantes"), ("dragón", "Dragones"), ("matadragones", "Dragones"),
-                ("terror nova", "Terror Nova"), ("dreadnova", "Terror Nova"),
-                ("fábulas", "Fábulas"),
-                ("gótico", "Gótico"),
-                ("cazador de cabezas", "Cazador de Cabezas"), ("headhunter", "Cazador de Cabezas"),
-                ("lucha libre", "Lucha Libre"), ("enmascarad", "Lucha Libre"), ("el león", "Lucha Libre"), ("el macho", "Lucha Libre"), ("el rayo", "Lucha Libre"),
-                ("merodeador", "Merodeadores y Celadores"), ("celador", "Merodeadores y Celadores"),
-                ("escuadrón omega", "Escuadrón Omega"),
-                ("de papel", "De Papel"), ("papercraft", "De Papel"),
-                ("faraón", "Guardián de las Arenas"), ("arenas", "Guardián de las Arenas"), ("sandstorm", "Guardián de las Arenas"),
-                ("pirata", "Aguas Turbias"), ("aguas turbias", "Aguas Turbias"), ("bucaner", "Aguas Turbias"), ("bilgewater", "Aguas Turbias"), ("corsari", "Aguas Turbias"), ("capitán", "Aguas Turbias"),
-                ("pretoriano", "Pretoriano"),
-                ("prehistórico", "Prehistórico"), ("dinosaurio", "Prehistórico"),
-                ("programa", "Programa"), ("program ", "Programa"),
-                ("riot", "Riot"),
-                ("agente secreto", "Agente Secreto"),
-                ("hoja relámpago", "Hoja Relámpago"), ("shockblade", "Hoja Relámpago"),
-                ("valquiria", "Valquirias de Acero"), ("almirante", "Valquirias de Acero"),
-                ("frenesí azucarado", "Frenesí Azucarado"), ("dulce", "Frenesí Azucarado"), ("sugar rush", "Frenesí Azucarado"),
-                ("juguete", "Juguetes"), ("toy", "Juguetes"), ("muñec", "Juguetes"),
-                ("vándalo", "Vándalos"), ("vandal", "Vándalos"),
-                ("rompemundos", "Rompemundos"), ("worldbreaker", "Rompemundos"),
-                ("juegos del cénit", "Juegos del Cénit"),
-                ("pacto quebrado", "Pacto Quebrado"), ("broken covenant", "Pacto Quebrado"),
-                ("corte feérica", "Corte Feérica"), ("faerie court", "Corte Feérica"),
-                ("demonios callejeros", "Demonios Callejeros"), ("street demon", "Demonios Callejeros"),
-                ("escamas celestiales", "Escamas Celestiales"), ("heavenscale", "Escamas Celestiales"),
-                ("emboscada primigenia", "Emboscada Primigenia"), ("primal ambush", "Emboscada Primigenia"),
-                ("aquelarre", "Aquelarre"), ("coven", "Aquelarre"),
-                ("bosqueviejo", "Bosqueviejo"), ("elderwood", "Bosqueviejo"),
-                ("eclipse", "Eclipse"),
-                ("arruinado", "Arruinados"), ("ruined", "Arruinados"),
-                ("centinela", "Centinelas de la Luz"), ("sentinel", "Centinelas de la Luz"),
-                ("meca", "Meca"), ("mecha ", "Meca"),
-                ("máquina de guerra", "Máquina de Guerra"), ("battlecast", "Máquina de Guerra"),
-                ("escarcha oscura", "Escarcha Oscura"), ("blackfrost", "Escarcha Oscura"),
-                ("infernal", "Infernal"), ("fuego sombrío", "Infernal"), ("volcánic", "Infernal"),
-                ("luz celestial", "Luz Celestial"), ("arclight", "Luz Celestial"), ("justicier", "Luz Celestial"),
-                ("portador del amanecer", "Amanecer y Anochecer"), ("portador de la noche", "Amanecer y Anochecer"), ("amanecer", "Amanecer y Anochecer"), ("anochecer", "Amanecer y Anochecer"), ("dawnbringer", "Amanecer y Anochecer"), ("nightbringer", "Amanecer y Anochecer"),
-                ("crystalis", "Crystalis Motus"),
-                ("fnc ", "eSports"), ("tpa ", "eSports"), ("skt t1", "eSports"), ("ssw ", "eSports"), ("ig ", "eSports"), ("fpx ", "eSports"), ("dwg ", "eSports"), ("edg ", "eSports"), ("drx ", "eSports"), ("t1 ", "eSports"),
-                ("supergaláctic", "Supergalácticos"),
-                ("psyops", "PsyOps"),
-                ("ilusión lunar", "Ilusión Lunar"),
-                ("rey dios", "Reyes Dioses"), ("dios rey", "Reyes Dioses"),
-                ("purgador", "Purgadores"),
-                ("inmortal", "Viaje Inmortal"), ("espada divina", "Viaje Inmortal"), ("báculo divino", "Viaje Inmortal"),
-                ("shurima", "Shurima"), ("freljord", "Freljord"), ("noxus", "Noxus"), ("noxian", "Noxus"), ("demacia", "Demacia"), ("jonia", "Jonia"),
-                ("reinos combatientes", "Reinos Combatientes"), ("warring kingdoms", "Reinos Combatientes")
+            // DICCIONARIO DE TEMÁTICAS EXTENDIDO
+            var tematicas = new Dictionary<string, string> {
+                { "proyecto", "PROYECTO" }, { "project", "PROYECTO" },
+                { "luna sangrienta", "Luna Sangrienta" }, { "blood moon", "Luna Sangrienta" },
+                { "estrella oscura", "Estrella Oscura" }, { "dark star", "Estrella Oscura" },
+                { "guardiana estelar", "Guardianas Estelares" }, { "star guardian", "Guardianas Estelares" },
+                { "forajid", "Forajidos" }, { "high noon", "Forajidos" },
+                { "k/da", "K/DA" }, { "true damage", "True Damage" }, { "pentakill", "Pentakill" }, { "heartsteel", "HEARTSTEEL" },
+                { "arcade", "Arcade" }, { "recreativa", "Arcade" }, { "jefe de batalla", "Arcade" },
+                { "piscina", "Pool Party" }, { "pool party", "Pool Party" }, { "veranieg", "Pool Party" },
+                { "florecer espiritual", "Florecer Espiritual" }, { "spirit blossom", "Florecer Espiritual" },
+                { "odisea", "Odisea" }, { "odyssey", "Odisea" },
+                { "academia de combate", "Academia de Combate" },
+                { "onda espacial", "Onda Espacial" }, { "empíreo", "Empíreo" }, { "peleador de almas", "Peleador de Almas" },
+                { "reinos mecha", "Reinos Mecha" }, { "rosa de cristal", "Rosa de Cristal" },
+                { "ciudad del crimen", "Ciudad del Crimen" }, { "mafios", "Ciudad del Crimen" },
+                { "noche de miedo", "Noche de Miedo" }, { "embrujada", "Halloween" }, { "zombi", "Halloween" },
+                { "invernal", "Invierno" }, { "nevado", "Invierno" }, { "santa", "Invierno" }, { "elfo", "Invierno" },
+                { "buscacorazones", "Buscacorazones" }, { "cariñosit", "Buscacorazones" },
+                { "victorios", "Victoriosa" }, { "campeonato", "Campeonato" }, { "conquistador", "Conquistador" },
+                { "hextech", "Hextech" }, { "prestigios", "Edición Prestigiosa" }, { "arcane", "Arcane" },
+                { "astronauta", "Astronauta" }, { "abeja", "Abejitas" }, { "porcelana", "Porcelana" },
+                { "cafetería", "Cafetería" }, { "chef", "Culinarios" }, { "panader", "Culinarios" },
+                { "dracomante", "Dracomantes" }, { "dragón", "Dragones" }, { "matadragones", "Dragones" },
+                { "merodeador", "Merodeadores y Celadores" }, { "celador", "Merodeadores y Celadores" },
+                { "escuadrón omega", "Escuadrón Omega" }, { "faraón", "Guardián de las Arenas" },
+                { "pirata", "Aguas Turbias" }, { "aguas turbias", "Aguas Turbias" },
+                { "aquelarre", "Aquelarre" }, { "bosqueviejo", "Bosqueviejo" },
+                { "arruinado", "Arruinados" }, { "centinela", "Centinelas de la Luz" },
+                { "meca", "Meca" }, { "máquina de guerra", "Máquina de Guerra" },
+                { "infernal", "Infernal" }, { "luz celestial", "Luz Celestial" },
+                { "portador del amanecer", "Amanecer y Anochecer" }, { "portador de la noche", "Amanecer y Anochecer" },
+                { "skt t1", "eSports" }, { "ssw", "eSports" }, { "ig ", "eSports" }, { "fpx", "eSports" }, { "dwg", "eSports" }, { "edg", "eSports" }, { "drx", "eSports" }, { "t1 ", "eSports" }
             };
 
             foreach (var champProp in champions) {
                 string champId = champProp.First["id"].ToString();
+                string champName = champProp.First["name"].ToString();
+                
+                // Mapeo dinámico de líneas basado en roles de Riot
+                string tags = champProp.First["tags"].ToString();
+                string linea = AsignarLinea(champId, tags);
+
+                await Task.Delay(25); // Evitar saturar API
+
                 var detailRaw = await client.GetStringAsync($"https://ddragon.leagueoflegends.com/cdn/{latestVersion}/data/es_ES/champion/{champId}.json");
                 var skinData = JObject.Parse(detailRaw)["data"][champId]["skins"];
 
                 foreach (var skin in skinData) {
                     string skinName = skin["name"].ToString();
                     string skinIdRiot = skin["id"].ToString();
-                    
-                    // ATRIBUTO 'NUM': La clave definitiva para filtrar aspectos base
                     string skinNum = skin["num"].ToString();
 
-                    // Si el número de la skin es 0, ES un aspecto base. Lo ignoramos.
-                    if (skinNum == "0") {
-                        continue; 
-                    }
+                    // FILTRO INFALIBLE: num == "0" es siempre la skin base
+                    if (skinNum == "0") continue; 
 
-                    // Si pasa el filtro, empezamos asumiendo que es una skin única
                     string tema = "Otras / Únicas";
-                    
                     foreach (var kw in tematicas) {
-                        if (skinName.ToLower().Contains(kw.key)) {
-                            tema = kw.theme;
+                        if (skinName.ToLower().Contains(kw.Key)) {
+                            tema = kw.Value;
                             break;
                         }
                     }
 
-                    await SaveSkinToDb(conn, skinIdRiot, skinName, tema);
+                    await SaveSkinToDb(conn, skinIdRiot, skinName, champName, linea, tema);
                 }
             }
         } catch (Exception ex) { Console.WriteLine("ERROR SYNC: " + ex.Message); }
     }
 
-    private async Task SaveSkinToDb(MySqlConnection conn, string skinId, string nombre, string tema) {
+    private string AsignarLinea(string champId, string tags) {
+        // Asignaciones manuales estrictas para los campeones más populares
+        var dictLineas = new Dictionary<string, string> {
+            { "Aatrox", "Top" }, { "Camille", "Top" }, { "Darius", "Top" }, { "Fiora", "Top" }, { "Garen", "Top" }, { "Irelia", "Top" }, { "Jax", "Top" }, { "Malphite", "Top" }, { "Mordekaiser", "Top" }, { "Nasus", "Top" }, { "Ornn", "Top" }, { "Renekton", "Top" }, { "Sett", "Top" }, { "Sion", "Top" }, { "Teemo", "Top" }, { "Urgot", "Top" }, { "Volibear", "Top" },
+            { "Amumu", "Jungle" }, { "Diana", "Jungle" }, { "Ekko", "Jungle" }, { "Evelynn", "Jungle" }, { "Fiddlesticks", "Jungle" }, { "Graves", "Jungle" }, { "Hecarim", "Jungle" }, { "JarvanIV", "Jungle" }, { "Kayn", "Jungle" }, { "KhaZix", "Jungle" }, { "Kindred", "Jungle" }, { "LeeSin", "Jungle" }, { "MasterYi", "Jungle" }, { "Nidalee", "Jungle" }, { "Nocturne", "Jungle" }, { "Nunu", "Jungle" }, { "Rengar", "Jungle" }, { "Sejuani", "Jungle" }, { "Shaco", "Jungle" }, { "Vi", "Jungle" }, { "Viego", "Jungle" }, { "XinZhao", "Jungle" }, { "Zac", "Jungle" },
+            { "Ahri", "Mid" }, { "Akali", "Mid" }, { "Anivia", "Mid" }, { "Annie", "Mid" }, { "Azir", "Mid" }, { "Cassiopeia", "Mid" }, { "Fizz", "Mid" }, { "Katarina", "Mid" }, { "LeBlanc", "Mid" }, { "Lissandra", "Mid" }, { "Malzahar", "Mid" }, { "Orianna", "Mid" }, { "Qiyana", "Mid" }, { "Ryze", "Mid" }, { "Sylas", "Mid" }, { "Syndra", "Mid" }, { "Talon", "Mid" }, { "TwistedFate", "Mid" }, { "Veigar", "Mid" }, { "Viktor", "Mid" }, { "Vladimir", "Mid" }, { "Yasuo", "Mid" }, { "Yone", "Mid" }, { "Zed", "Mid" }, { "Zoe", "Mid" },
+            { "Aphelios", "ADC" }, { "Ashe", "ADC" }, { "Caitlyn", "ADC" }, { "Draven", "ADC" }, { "Ezreal", "ADC" }, { "Jhin", "ADC" }, { "Jinx", "ADC" }, { "Kaisa", "ADC" }, { "Kalista", "ADC" }, { "KogMaw", "ADC" }, { "Lucian", "ADC" }, { "MissFortune", "ADC" }, { "Samira", "ADC" }, { "Sivir", "ADC" }, { "Tristana", "ADC" }, { "Twitch", "ADC" }, { "Varus", "ADC" }, { "Vayne", "ADC" }, { "Xayah", "ADC" }, { "Zeri", "ADC" },
+            { "Alistar", "Support" }, { "Bard", "Support" }, { "Braum", "Support" }, { "Janna", "Support" }, { "Karma", "Support" }, { "Leona", "Support" }, { "Lulu", "Support" }, { "Morgana", "Support" }, { "Nami", "Support" }, { "Nautilus", "Support" }, { "Pyke", "Support" }, { "Rakan", "Support" }, { "Rell", "Support" }, { "Renata", "Support" }, { "Senna", "Support" }, { "Seraphine", "Support" }, { "Sona", "Support" }, { "Soraka", "Support" }, { "Taric", "Support" }, { "Thresh", "Support" }, { "Yuumi", "Support" }, { "Zilean", "Support" }
+        };
+
+        if (dictLineas.ContainsKey(champId)) return dictLineas[champId];
+
+        // Fallback genérico si es un campeón muy nuevo o no listado arriba
+        if (tags.Contains("Marksman")) return "ADC";
+        if (tags.Contains("Support")) return "Support";
+        if (tags.Contains("Assassin")) return "Mid";
+        if (tags.Contains("Mage")) return "Mid";
+        if (tags.Contains("Tank")) return "Top";
+        return "Flex";
+    }
+
+    private async Task SaveSkinToDb(MySqlConnection conn, string skinId, string nombre_skin, string campeon, string linea, string tema) {
         int temaId;
         string queryTema = "INSERT IGNORE INTO Tematicas (nombre) VALUES (@tema); SELECT id_tematica FROM Tematicas WHERE nombre = @tema LIMIT 1;";
         using (var cmdTema = new MySqlCommand(queryTema, conn)) {
             cmdTema.Parameters.AddWithValue("@tema", tema);
-            var result = await cmdTema.ExecuteScalarAsync();
-            temaId = Convert.ToInt32(result);
+            temaId = Convert.ToInt32(await cmdTema.ExecuteScalarAsync());
         }
 
-        string querySkin = "INSERT IGNORE INTO Skins (id_skin_riot, id_tematica, nombre_skin) VALUES (@sid, @tid, @nombre)";
+        string querySkin = "INSERT IGNORE INTO Skins (id_skin_riot, id_tematica, nombre_skin, campeon, linea) VALUES (@sid, @tid, @nombre, @camp, @linea)";
         using (var cmdSkin = new MySqlCommand(querySkin, conn)) {
             cmdSkin.Parameters.AddWithValue("@sid", skinId);
             cmdSkin.Parameters.AddWithValue("@tid", temaId);
-            cmdSkin.Parameters.AddWithValue("@nombre", nombre);
+            cmdSkin.Parameters.AddWithValue("@nombre", nombre_skin);
+            cmdSkin.Parameters.AddWithValue("@camp", campeon);
+            cmdSkin.Parameters.AddWithValue("@linea", linea);
             await cmdSkin.ExecuteNonQueryAsync();
         }
     }
