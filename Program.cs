d16575bfe789ac9b074b9fa2b595248068a83fb7
CeleMyBeloved1404 <Tadeo.12972@gmail.com>
2026-02-24 14:25:06 -0300

    ww

--- Program.cs
+++ Program.cs
@@ -1,22 +1,12 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
-using System.Text;
-using Microsoft.OpenApi.Models; // Para que reconozca Swagger
+using System.Text; 
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Agrega esto para solucionar los errores de Swagger
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
